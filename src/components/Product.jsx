4bceb440e6268193440d33d50f6bbd26d886797d
anwarkvzk <anvarvzk521@gmail.com>
2022-10-26 12:41:03 +0530

    first commit

---
+++ src/components/Product.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import axios from "axios";
+
+function Product() {
+  
+  return <div className="products-container">Product</div>;
+}
+
+export default Product;
